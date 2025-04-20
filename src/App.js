0b7469ee720973c212468ecea17757ca6e22fd8d
Uy Tran <uytran@fmrp.vn>
2025-04-20 12:28:44 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./bgz.png";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App  bg-red-500">
+            <header className="App-header">
+                <h2 className="text-2xl font-bold text-gray-800 mb-10">QH88 - Trang Chủ QH88.Com Chính Thức 2025</h2>
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2 className="custom-text">Chào mừng bạn đến với QH88 – Nền tảng giải trí hàng đầu tại Việt Nam, nơi quy tụ những tựa game hấp dẫn nhất cùng hàng loạt ưu đãi đặc biệt dành riêng cho bạn!</h2>
+                <a href="https://698hghgg.poia.xyz/home/reg.html?inviteCode=11563084" target="_blank" rel="noopener noreferrer" className="no-underline">
+                    <button className="custom-button">
+                        <span className="font-medium">AN TOÀN ĐỂ MỞ</span>
+                    </button>
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
